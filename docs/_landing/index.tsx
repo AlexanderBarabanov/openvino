7d7a2392020fcea4eb919dfe2ba1478f58842b55
Colorado, Camilo <camilo.colorado@intel.com>
2026-06-18 14:50:18 +0200

    docs landing page

---
+++ docs/_landing/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@theme/Layout";
+
+export default function Home() {
+  return (
+    <Layout description="Open-source toolkit for deploying performant AI solutions">
+      <p>open vino landing page</p>
+    </Layout>
+  );
+}
